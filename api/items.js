b725b4bc5fadb2d92508deedd6dc89626d40010f
Matthew Victor <vicmcnabby@gmail.com>
2017-07-20 15:29:01 -0600

    added create route

--- api/items.js
+++ api/items.js
@@ -18,4 +18,13 @@ router.delete('/:id', (req, res, next)=>{
   });
 });
 
+router.post('/', function(req, res, next) {
+  queries.create({
+    name: req.body.name,
+    aisle: req.body.aisle,
+    photo_url: req.body.photo_url
+  }).then(function(result) {
+    res.json(result);
+  });
+});
 module.exports = router
--- db/queries.js
+++ db/queries.js
@@ -5,8 +5,13 @@ module.exports = {
 getAll() {
   return knex('items')
 },
+
 delete(id) {
   return knex('items').where('id', id).del()
+},
+
+create(item) {
+    return knex('items').insert(item, '*');
 }
 
 };
